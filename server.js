2afea7b73a7468dd2c7280f95b0415c1bb6f85a6
Dipanshu SHarma <ds737871@gmail.com>
2026-04-26 16:20:35 +0530

    Updated CORS for Netlify

--- server.js
+++ server.js
@@ -6,7 +6,13 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'https://your-netlify-site.netlify.app'
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 // Connect to MongoDB
